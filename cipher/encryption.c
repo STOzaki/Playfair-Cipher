fa4bbbd70d7f37de7875d614096cc87d777eef79
STOzaki <SOzaki19@cornellcollege.edu>
2017-10-25 08:00:12 -0500

    updated encryption
    add more comments

--- cipher/encryption.c
+++ cipher/encryption.c
@@ -4,16 +4,6 @@
 #define FALSE 0
 #define TRUE 1
 
-/*int inArray(int val, int *arr){
-    int i;
-    for (i=0; i < 26; i++) {
-        if (arr[i] == val)
-            return TRUE;
-    }
-    return FALSE;
-}
-*/
-
 int* search(char let0, char let1, char grid[5][5]){
   int* locations = malloc(sizeof(int) * 4);
   for(int i = 0; i < 5; i++){ // goes through each row
@@ -36,33 +26,34 @@ char* encrypt(char char1, char char2, char grid[5][5]){
   char* flip = malloc(sizeof(char) * 2);
   if(location[0] == location[2]){ // if they are on the same column
     // shift down
-    if(location[1] == 4){
-      location[1] = 0;
+    if(location[1] == 4){ // if the letter is at the bottom
+      location[1] = 0; // move to the top
     } else {
     location[1] = location[1] + 1;
   } // else
-    if(location[3] == 4){
-      location[3] = 0;
+    if(location[3] == 4){ // if the letter is at the bottom
+      location[3] = 0; // move to the top
     } else {
       location[3] = location[3] + 1;
     } // else
 
-  } else if(location[1] == location[3]){
+  } else if(location[1] == location[3]){ // if the are on the same row
     // shift right
-    if(location[0] == 4){
-      location[0] = 0;
+    if(location[0] == 4){ // if the letter is at the bottom
+      location[0] = 0; // move to the top
     } else {
     location[0] = location[0] + 1;
   } // else
-    if(location[2] == 4){
-      location[2] = 0;
+    if(location[2] == 4){ // if the letter is at the bottom
+      location[2] = 0; // move to the top
     } else {
       location[2] = location[2] + 1;
     }
   } else { // flip char1 and char2
     int tem = location[0]; // temporarily store location[0]
+    // the code below simply switches columns
     location[0] = location[2];
-    location[2] = tem; // give location[2] location[0]'s value
+    location[2] = tem;
   } // else
   flip[0] = grid[location[0]][location[1]];
   flip[1] = grid[location[2]][location[3]];
@@ -70,13 +61,13 @@ char* encrypt(char char1, char char2, char grid[5][5]){
   return flip;
 }
 
-void listing(char grid[5][5]){
+void listing(char grid[5][5]){ // this was used to debug by iterating throught the matrix
   for(int i = 0; i < 5; i++){
     for(int j = 0 ; j < 5; j++){
       printf("%c\n", grid[j][i]);
     } // for (j)
   } // for (i)
-}
+} // listing
 
 
 int main(int argc, char **argv){
@@ -87,9 +78,9 @@ int main(int argc, char **argv){
   int i = 0; // column
   int j = 0; // row
   char letter = 'A';
-  while(counter != 25){ // trying not to use two for loops, even though it is the same speed
+  while(counter != 25){ // creating a matrix with all letters except J
     if(letter!= 74){
-      if(counter % 5 == 0 && counter != 0){
+      if(counter % 5 == 0 && counter != 0){ // once we reach the end of the row, start on a new row
         j++;
         i = 0;
       } else {
@@ -98,7 +89,6 @@ int main(int argc, char **argv){
         } // if
       } // else
         grid[i][j] = letter;
-        //printf("%c\n", grid[i][j]);
         letter++;
         counter++;
     } else {
@@ -106,12 +96,10 @@ int main(int argc, char **argv){
     } // else
   }// while
 int count = 0;
-//char* da = encrypt('T','Y',grid);
 char* el = malloc(sizeof(char) * 2);
 char* result;
 FILE *fp = fopen("encrypted.txt","w+");
 while(scanf("%c", &c) != EOF){
-  //if(c != 32 && c != 10){ // if you type space it will stop the program.
     if(count % 2 == 1 && count != 0){
       el[1] = c;
       result = encrypt(el[0],el[1],grid);
@@ -125,26 +113,4 @@ while(scanf("%c", &c) != EOF){
 free(el);
 free(result);
 fclose(fp);
-
-  /*left[counter] = letter;
-  letter++;
-  printf("%c\n",left[counter]);
-  counter++;
-  */
-
-/*if(inArray(,*left) == 1){
-  printf("We found it!!");
-}*/
-
-/* maybe if I had more time
-while(count != 0){
-  int num = 97+(rand()%26);
-  //printf("%d\n",num);
-  if(letter != 106){ // if a is equal to
-    //if()
-  }
-  letter++;
-  count--;
-}
-*/
 } // main
