43dd6b5ef1eda1f2dae77da1cff058ca387f3c51
STOzaki <SOzaki19@cornellcollege.edu>
2017-12-09 17:50:37 -0600

    Update filterJ.c
    Add a commit that will make it easier to understand the code.

--- cipher/filterJ.c
+++ cipher/filterJ.c
@@ -5,6 +5,7 @@
 #define TRUE 1
 
 int main(int argc, char **argv){
+  //takes in a string or a file and create a output that has the appropriate parameters to run the Play-fair Cipher on this string/file
   char c;
   char last = 0;
   int counter = 0;
@@ -20,7 +21,7 @@ int main(int argc, char **argv){
     printf("%c", c);
     counter++; // add for every letter
   } // while
-  if(counter % 2 != 0){
+  if(counter % 2 != 0){ //if there are an odd number of letters including Xs, then add an X
     printf("%c", 'X');
   } // if
 } // main(int, char**)
